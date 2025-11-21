fe0d7fe03b3af562051d8bc6791d4538d9765d81
vasylenko-dmytro <dmytro.m.vasylenko@gmail.com>
2025-11-21 17:10:25 +0100

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.8"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.vasylenko"
+version = "0.0.1-SNAPSHOT"
+description = "Microservices demo project"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
