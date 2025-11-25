b2971fdd782da8565daf8a7e7767d668c4a4c06e
vasylenko-dmytro <dmytro.m.vasylenko@gmail.com>
2025-11-25 11:42:21 +0100

    Add Kafka docker config and modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,7 @@ rootProject.name = "instaservices"
 
 include("instagram-to-kafka-service")
 include("app-config-data")
+include("kafka")
+include("kafka:kafka-admin")
+include("kafka:kafka-model")
+include("kafka:kafka-producer")
